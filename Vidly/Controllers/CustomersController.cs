56317db08f283ab02b2682a8a32b3213d28e97eb
Richard Capil <Richard Capil>
2016-10-01 16:31:04 -0700

    section-2: added changes to Movies controller and finished adding rest of the requirements

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly List<Customer> _customers;
+        public CustomersController()
+        {
+            _customers = new List<Customer>()
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var viewModel = new CustomersIndexViewModel
+            {
+                Customers = _customers
+            };
+
+            return View(viewModel);
+        }
+
+        // GET: Customer Detail
+        [Route("customers/details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var customer = _customers.Find(x => x.Id == id);
+
+            if (customer == null)
+                return new HttpNotFoundResult();
+
+            return View(customer);
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,10 +10,26 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private readonly List<Movie> _movies;
+
+        public MoviesController()
+        {
+            _movies = new List<Movie>()
+            {
+                new Movie { Id = 1, Name = "Shrek!" },
+                new Movie { Id = 2, Name = "Wall-E" },
+            };
+        }
+
         // GET: Movies/
         public ActionResult Index()
         {
-            return View();
+            var viewModel = new MoviesIndexViewModel
+            {
+                Movies = _movies
+            };
+
+            return View(viewModel);
         }
 
         // GET: Movies/Random
