9f5f53960785ecfd4d08d59e18b37766334281fd
Richard Capil <Richard Capil>
2016-10-01 20:05:27 -0700

    section-3: removed hard coded customers and replaced it with DbContext calls

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
@@ -7,14 +8,16 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        private readonly List<Customer> _customers;
+        private ApplicationDbContext _context;
+
         public CustomersController()
         {
-            _customers = new List<Customer>()
-            {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
         }
 
         // GET: Customers
@@ -22,7 +25,7 @@ namespace Vidly.Controllers
         {
             var viewModel = new CustomersIndexViewModel
             {
-                Customers = _customers
+                Customers = _context.Customers.ToList()
             };
 
             return View(viewModel);
@@ -32,10 +35,10 @@ namespace Vidly.Controllers
         [Route("customers/details/{id}")]
         public ActionResult Details(int id)
         {
-            var customer = _customers.Find(x => x.Id == id);
+            var customer = _context.Customers.SingleOrDefault(x => x.Id == id);
 
             if (customer == null)
-                return new HttpNotFoundResult();
+                return HttpNotFound();
 
             return View(customer);
         }
