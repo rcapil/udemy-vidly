68e474a6649e6e23da7d84571c546aaa87f3f797
Richard Capil <Richard Capil>
2016-10-01 19:54:00 -0700

    section-3: applied dataannotation to Customer Name property

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
